b21899c12d88f6715a3e9194a4161a988a5ebe23
Michał Zawada <m.zawada@controlexpert.com>
2023-12-13 21:13:56 +0100

    changes in day 3

--- day3/day3.c
+++ day3/day3.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LEN_OF_LINE 100
+
 struct Position {
     int x;
     int y;
 };
 
 size_t countLines(FILE *file) {
-    
+
     size_t length = 0;
     size_t pos = ftell(file);
     fseek(file, 0, SEEK_SET);
-    char line[100];
-    while(fgets(line,100,file)) {
+    char line[LEN_OF_LINE];
+
+    while(fgets(line,LEN_OF_LINE,file)) {
         length++;
     }
+
     fseek(file, pos, SEEK_SET);
     return length;
+
 }
+
+char createBitMatrix(FILE *file, size_t length) {
+    
+}
+
 
 int problem1(FILE *file) {
 
     size_t length = countLines(file);
     
-    char line[100], matrix[100][length];
-    struct Position positions[100*length];
+    char line[LEN_OF_LINE], matrix[LEN_OF_LINE][length];
+    struct Position positions[LEN_OF_LINE*length];
 
-    while (fgets(line,100,file)) {
-        int n = strcspn(line, "*&");
+    while (fgets(line,LEN_OF_LINE,file)) {
+        int n = strcspn(line, "!@#$%^&*");
         printf("%s", line);
     }
     printf("%zu", length);
